72923fb8f6520e7f29015039ed5ea6bc121278f2
Haitian Nick <haitiannick@gmail.com>
2024-06-18 01:57:40 -0400

    Select Table to view

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const mysql = require('mysql2');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -8,16 +9,31 @@ const app = express();
 app.use(express.urlencoded({extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Time2win_2024',
+    database: 'election'
+  },
+  console.log("Connected to the election database")
+);
+
 app.get('/', (req, res) => {
     res.json({
       message: 'Hello World'
     });
   });
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 app.use((req, res) => {
   res.status(404).end();
 })
 
-  app.listen(3001, () => {
-    console.log(`Server running on port 3001`);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
